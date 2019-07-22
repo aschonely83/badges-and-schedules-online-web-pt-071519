1a650448ff30a1c80324027cf8ed2da4021eaae9
Adam Schonely <aschonely@gmail.com>
2019-07-22 23:21:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,8 @@
-
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   print "Hello, my name is #{name}."
 end 
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
    
  
